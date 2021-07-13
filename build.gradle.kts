206ae63a667bf4ee4738383bc33598f25460720b
myoun <aiden080605@gmail.com>
2021-07-13 16:17:46 +0900

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.20"
+}
+
+group = "kr.myoung2"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
